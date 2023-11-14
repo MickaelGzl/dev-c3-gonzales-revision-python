7de8234d057d2a53015c1870d8a3c852010d1be3
Mickael Gonzales <mickael.gonzales@hotmail.com>
2023-11-14 16:41:59 +0100

    each turn display info of max, min and average of array calculated

--- ex4.py
+++ ex4.py
@@ -1,7 +1,24 @@
 import math
+import random
 
 
 numbers = []
-for i in range(0, 10):
-    numbers.append(math.random() * 100)
+for i in range(10):
+    numbers.append(math.floor(random.random() * 100))
+
+print(numbers)
+
+total = numbers[0]
+max = numbers[0]
+min = numbers[0]
+
+for i in range(1, len(numbers)):
+    total = total + numbers[i]
+    moy = total / (i+1)
+    if numbers[i] < min:
+        min = numbers[i]
+    elif numbers[i] > max:
+        max = numbers[i]
+    print(f"opération {i}: moyenne: {moy}, maximum: {max}, min: {min}")
+
 
