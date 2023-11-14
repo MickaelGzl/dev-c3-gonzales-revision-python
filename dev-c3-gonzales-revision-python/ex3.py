b042bfc5ba8b4c60cf24112fb75af30813afa015
Mickael Gonzales <mickael.gonzales@hotmail.com>
2023-11-14 16:19:29 +0100

    return value of the evaluation

--- dev-c3-gonzales-revision-python/ex3.py
+++ dev-c3-gonzales-revision-python/ex3.py
@@ -1,6 +1,5 @@
 import re
 
-
 def add(a, b):
     return a + b
 
@@ -17,47 +16,56 @@ def verifyInput(input):
     return re.match(r'^[0-9\.\+\-\*\/]+$', input)
 
 def evalUserInput(input):
-    if not verifyInput(input):
-        raise ValueError("Votre entrée contient des caractères non valide pour une opération")
-    operations = []
-    actual_number = ''
-    for index, c in enumerate(input):
-        if c.isdigit() or c == '.':
-            actual_number += c
-        else:
-            operations.append(actual_number)
-            operations.append(c)
-            actual_number = ''
+    try:
+        if not verifyInput(input):
+            raise ValueError("Votre entrée contient des caractères non valide pour une opération")
+        operations = []
+        actual_number = ''
+        for index, c in enumerate(input):
+            if c.isdigit() or c == '.':
+                actual_number += c
+            else:
+                operations.append(actual_number)
+                operations.append(c)
+                actual_number = ''
 
-        if index == len(input) - 1:
-            operations.append(actual_number)
+            if index == len(input) - 1:
+                operations.append(actual_number)
 
-    i = 0
+        i = 0
 
-    # do first all * and / opérations. remove numbers and op and add result
+        # do first all * and / opérations. remove numbers and op and add result
 
-    while i < len(operations) -1:
-        if operations[i] == '*':
-            result = multiply(float(operations[i - 1]), float(operations[i + 1]))
-            del operations[i - 1: i+2]
-            operations.insert(i-1, result)
-        if operations[i] == '/':
-            if(operations[i + 1] == 0):
-                raise ValueError("Impossible de réaliser une divivion par 0")
-            result = divise(float(operations[i - 1]), float(operations[i + 1]))
-            del operations[i - 1:i+2]
-            operations.insert(i-1, result)
-        else:
-            i = i+1
-    
-    
-    
+        while i < len(operations) -1:
+            if operations[i] == '*':
+                result = multiply(float(operations[i - 1]), float(operations[i + 1]))
+                del operations[i - 1: i+2]
+                operations.insert(i-1, result)
+            if operations[i] == '/':
+                if(operations[i + 1] == 0):
+                    raise ValueError("Impossible de réaliser une divivion par 0")
+                result = divise(float(operations[i - 1]), float(operations[i + 1]))
+                del operations[i - 1:i+2]
+                operations.insert(i-1, result)
+            else:
+                i = i+1
 
-    return operations
+        # now do each operation 1 by 1 until we have only 1 number
 
-            
+        while len(operations) > 1:
+            result = None
+            if operations[1] == '+':
+                result = add(float(operations[0]), float(operations[2]))
+                del operations[:3]
+                operations.insert(0, result)
+            elif operations[1] == '-':
+                result = substract(float(operations[0]), float(operations[2]))
+                del operations[:3]
+                operations.insert(0, result)
 
-    
+        return operations[0]
+    except ValueError as e:
+        print(e)
 
 userInput = input('Entrer une opération: ')
 print(evalUserInput(userInput))
