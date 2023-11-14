6725e31281fd11058161dcf264095ddfb8f91860
Mickael Gonzales <mickael.gonzales@hotmail.com>
2023-11-14 17:23:53 +0100

    add function to verify note is valid

--- dev-c3-gonzales-revision-python/exo/ex7.py
+++ dev-c3-gonzales-revision-python/exo/ex7.py
@@ -1,3 +1,6 @@
+import re
+
+
 def findStudentWithBestNote(students):
     bestNote = 0
     bestStudents = []
@@ -9,3 +12,23 @@ def findStudentWithBestNote(students):
             bestStudents.append(key)
     return sorted(bestStudents)[0]
 
+students = {}
+
+def enterValidNote(value):
+    if not re.match(r'^[0-9.]+$', value):
+        print("Valeur invalide pour une node d'étudiant")
+        enterValidNote(input('Entrez sa note: '))
+    return value
+    
+
+while True:
+    studentName = input("Entrez le nom de l'étudiant: ")
+    studentNote = enterValidNote(input('Entrez sa note: '))
+
+    students[studentName] = studentNote
+    addAnotherEntry = input("Ajouter un autre étudiant (y/n): ")
+    if addAnotherEntry == 'n':
+        break
+    
+
+print(students)
